cde70c82db04dd40959aa7e15a37debcff4615db
EC2 Default User <ec2-user@ip-172-31-34-254.us-east-2.compute.internal>
2020-02-17 21:03:32 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,28 +11,29 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @category = params[:category]
-    @movies = Movie.all.order(params[:sort_by])
+    
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:title => :asc}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:release_date => :asc}, 'hilite'
+    end
     @all_ratings = Movie.all_ratings
-    @ratings = params[:ratings]
-    if !@ratings
-      @ratings = Hash.new
-      @all_ratings.each do |rating|
-        @ratings[rating] = 1
-      end
-    end
-    if @category and @ratings
-      @movies = Movie.where(:rating => @ratings.keys).order(@category)
-    elsif @ratings
-      @movies = Movie.where(:rating => @ratings.keys)
-    elsif @category
-      @movies = Movie.order(@category)
-    else
-      @movies = Movie.all
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
 
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
-
+  
   def new
     # default: render 'new' template
   end
